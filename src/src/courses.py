dd0fe3e94145fcf0e8cdc1331bc88e37e8fd0ebc
kaviyatech07 <kaviyaselvakumar07@gmail.com>
2026-06-26 18:05:52 +0530

    Create courses.py

---
+++ src/src/courses.py
@@ -0,0 +1,19 @@
+courses = {
+    "AI": [
+        "Python for AI",
+        "Machine Learning Basics",
+        "Deep Learning Fundamentals"
+    ],
+
+    "Data Science": [
+        "Python for Data Science",
+        "SQL Basics",
+        "Data Visualization"
+    ],
+
+    "Software Development": [
+        "Python Programming",
+        "Git & GitHub",
+        "Object-Oriented Programming"
+    ]
+}
